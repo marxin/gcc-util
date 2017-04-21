4c092f08b746797040a31287719a240236956c04
marxin <mliska@suse.cz>
2017-04-21 12:25:07 +0200

    Add first version of filter.

--- boilerplate/gcc_testsuite_runner.py
+++ boilerplate/gcc_testsuite_runner.py
@@ -28,6 +28,8 @@ if parallelism > parallelism_limit:
 make_cmd = 'nice make -j' + str(parallelism)
 make_test_cmd = 'nice make check -k -j' + str(parallelism)
 
+ignored = set(['guality/nrv-1.c',  'guality/param-2.c', 'guality/param-3.c', 'guality/pr36728-1.c', 'guality/pr36728-2.c', 'guality/pr41353-1.c', 'guality/pr41616-1.c', 'guality/pr43051-1.c', 'guality/pr45882.c', 'guality/pr54200.c', 'guality/pr54519-1.c', 'guality/pr54519-2.c', 'guality/pr54519-3.c', 'guality/pr54519-4.c', 'guality/pr54519-5.c', 'guality/pr54551.c', 'guality/pr54693-2.c', 'guality/pr54693.c', 'guality/pr58791-2.c', 'guality/pr68860-1.c', 'guality/pr78726.c', 'guality/sra-1.c', 'guality/vla-1.c'])
+
 def tail(message):
   lines = message.split('\n')
   return '\n'.join(lines[-50:])
@@ -174,13 +176,17 @@ class GccTester:
         lines = r.split('\n')
 
         failures = [x for x in lines if x.startswith('FAIL')]
+        known_failures = [x for x in failures if x in ignored]
+        failures = [x for x in failures if not x in ignored]
         xfail_count = len([x for x in lines if x.startswith('XFAIL')])
         pass_count = len([x for x in lines if x.startswith('PASS')])
 
         self.log('PASS count: %d' % pass_count)
         self.log('XFAIL count: %d' % xfail_count)
         self.log('FAIL count: %d' % len(failures))
-        self.messages += ['Compare logs', '\n'.join(failures)]
+        self.messages += ['=== FAILURES ===', '\n'.join(failures)]
+        self.log('Known false FAIL count: %d' % len(known_failures))
+        self.messages += ['=== False positive failures ===', '\n'.join(known_failures)]
 
     def run(self):
         # core of the script

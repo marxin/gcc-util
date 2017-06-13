4128900c293256caed01e1502bd1392718e593b6
marxin <mliska@suse.cz>
2017-06-13 14:13:45 +0200

    Adjust email subject.

--- boilerplate/gcc_testsuite_runner.py
+++ boilerplate/gcc_testsuite_runner.py
@@ -110,7 +110,7 @@ class GccTester:
         sender = 'mliska+tester@foxlink.cz'
         recipient = 'mliska@suse.cz'
 
-        subject = 'GCC tester email (%s): %s ' % (self.revision, 'FAILURE' if failure else 'SUCCESS')
+        subject = 'Revision %s: %s : ' % (self.revision, 'FAILURE' if failure else 'SUCCESS')
         if not failure:
             subject += 'ALL TEST PASSED' if failed_tests == 0 else '%d TESTS FAILED' % failed_tests
 

0109d61c338dc18985b636eade65230b128e74f7
marxin <mliska@suse.cz>
2018-02-16 14:31:43 +0100

    Update bisect script.

--- boilerplate/gcc-bisect.sh
+++ boilerplate/gcc-bisect.sh
@@ -11,25 +11,24 @@ function clean {
 
 # apply patch
 clean
-patch -p1 < /tmp/p
 
 rm -rf $TMPDIR
 mkdir $TMPDIR
 cd $TMPDIR
 
 date
-$SOURCE/configure --enable-languages=c,c++ --disable-bootstrap --disable-libsanitizer --target=m32c-rtems &>> $LOG || exit 255 
-make -j9 CXXFLAGS="-O0 -fpermissive" CFLAGS="-O0" all-host &>> $LOG || exit 125
+$SOURCE/configure --enable-languages=c,c++ --disable-bootstrap --disable-libsanitizer --target=ppc64le-linux-gnu  &>> $LOG || exit 255 
+nice make -j9 CXXFLAGS="-O0 -fpermissive" CFLAGS="-O0" all-host &>> $LOG || exit 125
 date
 
-$TMPDIR/gcc/xgcc -B$TMPDIR/gcc /tmp/main.c ~/Programming/testcases/pr64546.c -mcpu=m32cm -Os -c 2>&1 | grep 'internal compiler error'
+$TMPDIR/gcc/xg++ -B$TMPDIR/gcc /tmp/ice.c  --param ira-max-conflict-table-size=0 -O3 2>&1 | grep 'internal compiler error'
 
 if test $? = 0; then
   echo ICE
   clean
-  exit 1
+  exit 0
 fi
 
 echo OK
 clean
-exit 0
+exit 1

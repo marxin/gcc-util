bda5021f6890df797a7659c8b5e97d553cfc5a47
marxin <mliska@suse.cz>
2018-11-20 19:54:20 +0100

    Update script.

--- boilerplate/gcc-bisect.sh
+++ boilerplate/gcc-bisect.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-TMPDIR=/dev/shm/gcc-objdir
+TMPDIR=/dev/shm/gcc-objdir-bisect
 LOG=/tmp/gcc.log
-SOURCE=~/Programming/gcc2
+SOURCE=~/Programming/gcc
 
 function clean {
   cd $SOURCE
@@ -17,11 +17,11 @@ mkdir $TMPDIR
 cd $TMPDIR
 
 date
-$SOURCE/configure --enable-languages=c,c++ --disable-bootstrap --disable-libsanitizer --target=ppc64le-linux-gnu  &>> $LOG || exit 255 
+$SOURCE/configure --enable-languages=c,c++ --disable-bootstrap --disable-libsanitizer --target=aarch64-linux-gnu --without-isl  &>> $LOG || exit 255
 nice make -j9 CXXFLAGS="-O0 -fpermissive" CFLAGS="-O0" all-host &>> $LOG || exit 125
 date
 
-$TMPDIR/gcc/xg++ -B$TMPDIR/gcc /tmp/ice.c  --param ira-max-conflict-table-size=0 -O3 2>&1 | grep 'internal compiler error'
+$TMPDIR/gcc/xg++ -B$TMPDIR/gcc -O2 /tmp/Unified.ii  2>&1 | grep 'contains_placeholder_p'
 
 if test $? = 0; then
   echo ICE

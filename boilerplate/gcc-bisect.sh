ef6ea7f2306c7c8a8c954ae18284958aa79e947f
marxin <mliska@suse.cz>
2019-01-02 08:34:25 +0100

    Another example in gcc-bisect.sh.

--- boilerplate/gcc-bisect.sh
+++ boilerplate/gcc-bisect.sh
@@ -2,7 +2,7 @@
 
 TMPDIR=/dev/shm/gcc-objdir-bisect
 LOG=/tmp/gcc.log
-SOURCE=~/Programming/gcc
+SOURCE=~/Programming/gcc2
 
 function clean {
   cd $SOURCE
@@ -17,18 +17,18 @@ mkdir $TMPDIR
 cd $TMPDIR
 
 date
-$SOURCE/configure --enable-languages=c,c++ --disable-bootstrap --disable-libsanitizer --target=aarch64-linux-gnu --without-isl  &>> $LOG || exit 255
+$SOURCE/configure --enable-languages=c,c++ --disable-bootstrap --disable-libsanitizer --target=arm-linux-gnueabi --without-isl --with-as=/usr/bin/arm-suse-linux-gnueabi-as &>> $LOG || exit 255
 nice make -j9 CXXFLAGS="-O0 -fpermissive" CFLAGS="-O0" all-host &>> $LOG || exit 125
 date
 
-$TMPDIR/gcc/xg++ -B$TMPDIR/gcc -O2 /tmp/Unified.ii  2>&1 | grep 'contains_placeholder_p'
+$TMPDIR/gcc/xg++ -B$TMPDIR/gcc -Ofast /tmp/ice.ii 2>&1 | grep 'gimplify_expr'
 
 if test $? = 0; then
   echo ICE
   clean
-  exit 0
+  exit 1
 fi
 
 echo OK
 clean
-exit 1
+exit 0

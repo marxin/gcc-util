bc8b2b739b962abd3206b79fdeded5531aefc093
Martin Liska <mliska@suse.cz>
2019-08-08 14:10:32 +0200

    Enhance script

--- boilerplate/bisect-gcc.sh
+++ boilerplate/bisect-gcc.sh
@@ -3,6 +3,9 @@
 TMPDIR=/dev/shm/gcc-objdir-bisect
 LOG=/tmp/gcc.log
 SOURCE=~/Programming/gcc2
+SOURCEFILE="/home/marxin/Programming/gcc/gcc/testsuite/gfortran.dg/vect/cost-model-pr34445.f"
+OPTIONS="-c -misel -O2 -fstack-protector -funroll-all-loops -fno-sched-pressure -fno-tree-ch -fno-tree-forwprop -fno-tree-ter"
+GREP="fix_reg_equiv_init"
 
 function clean {
   cd $SOURCE
@@ -17,11 +20,11 @@ mkdir $TMPDIR
 cd $TMPDIR
 
 date
-$SOURCE/configure --enable-languages=c,c++ --disable-bootstrap --disable-libsanitizer --target=arm-linux-gnueabi --without-isl --with-as=/usr/bin/arm-suse-linux-gnueabi-as &>> $LOG || exit 255
+$SOURCE/configure --enable-languages=c,c++,fortran --disable-bootstrap --disable-libsanitizer --without-isl --target=ppc-linux-gnu &>> $LOG || exit 255
 nice make -j9 CXXFLAGS="-O0 -fpermissive" CFLAGS="-O0" all-host &>> $LOG || exit 125
 date
 
-$TMPDIR/gcc/xg++ -B$TMPDIR/gcc -Ofast /tmp/ice.ii 2>&1 | grep 'gimplify_expr'
+$TMPDIR/gcc/xg++ -B$TMPDIR/gcc $SOURCEFILE $OPTIONS 2>&1 | grep $GREP
 
 if test $? = 0; then
   echo ICE
